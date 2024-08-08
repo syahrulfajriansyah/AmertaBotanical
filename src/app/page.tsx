48d3c287c9c62466ed45712282bd0b2386d72408
syahrulfajriansyah <syahrulisbani1@gmail.com>
2024-08-09 03:24:43 +0700

    Update HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
 import Hero from '../components/Hero/Hero';
-import CompanyOverview from '../components/CompanyOverview/CompanyOverview';
-import ProductsServices from '../components/ProductsServices/ProductsServices';
-import Testimonials from '../components/Testimonials/Testimonial'; // Pastikan nama komponen benar
 import VideoSection from '../components/VideoSection/VideoSection'; // Import the VideoSection component
 import ContactUs from '../components/contact/contactUs';
 
@@ -9,9 +6,6 @@ const HomePage = () => {
   return (
     <>
       <Hero />
-      <CompanyOverview />
-      <ProductsServices />
-      <Testimonials />
       <VideoSection />
       <ContactUs/>
     </>
